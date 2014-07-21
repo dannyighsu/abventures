584b535796b8281a9dc809788916660f0bcefa5c
Danny Hsu <danny@kloudless.com>
2014-07-21 02:18:40 -0700

    added 12factor gem for deploy

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,5 @@ module ABVentures
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     config.assets.enabled = true
-    config.assets.compile = true  
   end
 end
